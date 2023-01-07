3ca406f6c5b0e33ada834092c06cb52313c6c7bc
Luke <lpmaho@gmail.com>
2023-01-07 23:50:56 +0900

    upated donation

--- donation/contract/src/donation.rs
+++ donation/contract/src/donation.rs
@@ -1,79 +1,200 @@
 use crate::Contract;
 use crate::ContractExt;
 
-use near_sdk::serde::Serialize;
+use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, near_bindgen, AccountId, Promise, Balance};
+use near_sdk::collections::UnorderedMap;
 use near_sdk::json_types::U128;
+use near_sdk::serde::Serialize;
+use near_sdk::{
+    env, ext_contract, log, near_bindgen, AccountId, Balance, Gas, Promise, PromiseError,
+};
 
+pub const TGAS: u64 = 1_000_000_000_000;
 pub const STORAGE_COST: u128 = 1_000_000_000_000_000_000_000;
+
+// Validator interface, for cross-contract calls
+#[ext_contract(nft_creator)]
+trait NftCreator {
+    fn get_owner_by_token_id(&self, token_id: TokenId) -> AccountId;
+}
+
+#[ext_contract(ext_self)]
+trait ExtSelf {
+    fn privilege_check(&self, token_id: TokenId) -> Promise;
+}
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Donation {
-  pub account_id: AccountId, 
-  pub total_amount: U128,
+    pub account_id: AccountId,
+    pub total_amount: U128,
 }
 
 #[near_bindgen]
 impl Contract {
-  #[payable] // Public - People can attach money
-  pub fn donate(&mut self) -> U128 {
-    // Get who is calling the method and how much $NEAR they attached
-    let donor: AccountId = env::predecessor_account_id();
-    let donation_amount: Balance = env::attached_deposit();
+    #[payable] 
+    pub fn donate(&mut self) -> U128 {
+        // Get who is calling the method and how much $NEAR they attached
+        let donor: AccountId = env::predecessor_account_id();
+        let donation_amount: Balance = env::attached_deposit();
 
-    let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
+        let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
 
-    let to_transfer: Balance = if donated_so_far == 0 {
-      // This is the user's first donation, lets register it, which increases storage
-      assert!(donation_amount > STORAGE_COST, "Attach at least {} yoctoNEAR", STORAGE_COST);
+        let to_transfer: Balance = if donated_so_far == 0 {
+          
+            assert!(
+                donation_amount > STORAGE_COST,
+                "Attach at least {} yoctoNEAR",
+                STORAGE_COST
+            );
 
-      // Subtract the storage cost to the amount to transfer
-      donation_amount - STORAGE_COST
-    }else{
-      donation_amount
-    };
+            donation_amount - STORAGE_COST
+        } else {
+            donation_amount
+        };
 
-    // Persist in storage the amount donated so far
-    donated_so_far += donation_amount;
-    self.donations.insert(&donor, &donated_so_far);
-    
-    log!("Thank you {} for donating {}! You donated a total of {}", donor.clone(), donation_amount, donated_so_far);
-    
-    // Send the money to the beneficiary
-    Promise::new(self.beneficiary.clone()).transfer(to_transfer);
+        // Persist in storage the amount donated so far -- do we need to prevent
+        // hitting a max storage amoutnt
+        donated_so_far += donation_amount;
+        self.donations.insert(&donor, &donated_so_far);
 
-    // Return the total amount donated so far
-    U128(donated_so_far)
-  }
+        log!(
+            "Thank you {} for donating {}! You donated a total of {}",
+            donor.clone(),
+            donation_amount,
+            donated_so_far
+        );
 
-  // Public - get donation by account ID
-  pub fn get_donation_for_account(&self, account_id: AccountId) -> Donation {
-    Donation {
-      account_id: account_id.clone(),
-      total_amount: U128(self.donations.get(&account_id).unwrap_or(0))
+        log!(
+            "for reference, this is the transfer amount: {}. 
+          You can withdrawal/trasfer it by calling the withdrawal method",
+            to_transfer
+        );
+
+        U128(donated_so_far)
     }
-  }
 
-  // Public - get total number of donors
-  pub fn number_of_donors(&self) -> u64 {
-    self.donations.len()
-  }
+    //HOW MUCH GAS IS USED ??? check rpc
+    pub fn privilege_check(&mut self) -> Promise {
+        let promise = nft_creator::ext(self.nft_deployed_account.clone())
+            .with_static_gas(Gas(5 * TGAS))
+            .get_owner_by_token_id(self.token_id.clone());
 
-  // Public - paginate through all donations on the contract
-  pub fn get_donations(&self, from_index: Option<U128>, limit: Option<u64>) -> Vec<Donation> {
-    //where to start pagination - if we have a from_index, we'll use that - otherwise start from 0 index
-    let start = u128::from(from_index.unwrap_or(U128(0)));
+        return promise.then(
+            Self::ext(env::current_account_id())
+                .with_static_gas(Gas(5 * TGAS))
+                .privilege_check_callback(),
+        );
+    }
 
-    //iterate through donation
-    self.donations.keys()
-      //skip to the index we specified in the start variable
-      .skip(start as usize) 
-      //take the first "limit" elements in the vector. If we didn't specify a limit, use 50
-      .take(limit.unwrap_or(50) as usize) 
-      .map(|account| self.get_donation_for_account(account))
-      //since we turned map into an iterator, we need to turn it back into a vector to return
-      .collect()
-  }
+    #[private]
+    pub fn privilege_check_callback(
+        &mut self,
+        #[callback_result] call_result: Result<AccountId, PromiseError>,
+    ) {
+        if call_result.is_err() {
+            log!("There was an error querying the token owner");
+            /*  return "Error".to_string(); */
+        }
+
+        let account_id: AccountId = call_result.unwrap();
+        log!(
+            "from privilege_check_callback, token holder account is {} and privileged_account is currently set to {}",
+            account_id,
+            self.privileged_account
+        );
+        self.privileged_account = account_id;
+        log!(
+            "from privilege_check_callback, privileged_account is now set to {}",
+            self.privileged_account
+        );
+    }
+
+    //#[privileged_func_call]
+    pub fn withdrawal(&mut self) -> U128 {
+        
+        let caller = env::predecessor_account_id();
+        if self.privileged_account.clone() != caller {
+            panic!(
+                "method access denied - no NFT in account - trying running privilege check first"
+            );
+        }
+
+        let mut withdrawal_amount: u128 = 0;
+
+        let values = self.donations.values();
+
+        for d in values {
+            let val: u128 = u128::from(d);
+            withdrawal_amount += val;
+            log!(
+                "incrementing withdrawal amount, currently its {}",
+                withdrawal_amount
+            );
+        }
+
+        log!(
+            "For reference the folded withdrawal_amount is {}",
+            withdrawal_amount
+        );
+
+        assert!(
+            withdrawal_amount > STORAGE_COST,
+            "The donation amount is {}, wait until its more than {}",
+            withdrawal_amount,
+            STORAGE_COST
+        );
+
+        let transfer_amount: Balance = Balance::from(withdrawal_amount);
+        log!("for ref, the transfer amount is {}", transfer_amount);
+
+        let beneficiary = self.beneficiary.clone();
+
+        //change to ValidAccountId check????????
+        assert!(
+            beneficiary == caller,
+            "Beneficiary is not the caller, meaning transfer could fail"
+        );
+
+        Promise::new(beneficiary).transfer(transfer_amount);
+
+      
+        //UnorderedMaps need to be initialized with unique keys (as byte strings)
+        //if not, the contract will hang so thats occuring here --
+        // you could also apply a sha256 hash to a random value  
+        let block_height = env::block_height();
+        let bytes = block_height.to_be_bytes();
+        self.donations = UnorderedMap::new(bytes.as_ref());
+        log!("self.donations is currently {:?}", &self.donations);
+
+        U128(withdrawal_amount)
+    }
+
+    pub fn get_donation_for_account(&self, account_id: AccountId) -> Donation {
+        Donation {
+            account_id: account_id.clone(),
+            total_amount: U128(self.donations.get(&account_id).unwrap_or(0)),
+        }
+    }
+
+    pub fn number_of_donors(&self) -> u64 {
+        self.donations.len()
+    }
+
+    pub fn get_donations(&self, from_index: Option<U128>, limit: Option<u64>) -> Vec<Donation> {
+        //where to start pagination - if we have a from_index, we'll use that - otherwise start from 0 index
+        let start = u128::from(from_index.unwrap_or(U128(0)));
+
+        //iterate through donation
+        self.donations
+            .keys()
+            //skip to the index we specified in the start variable
+            .skip(start as usize)
+            //take the first "limit" elements in the vector. If we didn't specify a limit, use 50
+            .take(limit.unwrap_or(50) as usize)
+            .map(|account| self.get_donation_for_account(account))
+            //since we turned map into an iterator, we need to turn it back into a vector to return
+            .collect()
+    }
 }
--- donation/contract/src/lib.rs
+++ donation/contract/src/lib.rs
@@ -1,102 +1,92 @@
+use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{env, ext_contract, log, near_bindgen, AccountId};
 
 mod donation;
+
+#[ext_contract(nft_creator)]
+trait NftCreator {
+    fn get_owner_by_token_id(&self, token_id: TokenId) -> AccountId;
+}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-  pub beneficiary: AccountId,
-  pub donations: UnorderedMap<AccountId, u128>,
+    //person receiving donation
+    pub beneficiary: AccountId,
+    pub donations: UnorderedMap<AccountId, u128>,
+    //extract into constant
+    pub nft_deployed_account: AccountId,
+    pub privileged_account: AccountId,
+    pub token_id: TokenId,
 }
 
 impl Default for Contract {
-  fn default() -> Self {
-    Self{
-      beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
-      donations: UnorderedMap::new(b"d"),
+    fn default() -> Self {
+        Self {
+            beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
+            donations: UnorderedMap::new(b"d"),
+            nft_deployed_account: AccountId::new_unchecked(
+                "nftcreator.lukemlabs.testnet".to_string(),
+            ),
+            privileged_account: AccountId::new_unchecked("lukemlabs.testnet".to_string()),
+            token_id: "0".to_string(),
+        }
     }
-  }
 }
 
 #[near_bindgen]
 impl Contract {
-  #[init]
-  #[private] // Public - but only callable by env::current_account_id()
-  pub fn init(beneficiary: AccountId) -> Self {
-    Self {
-      beneficiary,
-      donations: UnorderedMap::new(b"d"),
+    #[init]
+    #[private]
+    pub fn init(
+        beneficiary: AccountId,
+        nft_deployed_account: AccountId,
+        token_id: TokenId,
+    ) -> Self {
+        let holder = env::predecessor_account_id().clone();
+        Self {
+            beneficiary,
+            donations: UnorderedMap::new(b"d"),
+            nft_deployed_account,
+            privileged_account: holder,
+            token_id,
+        }
     }
-  }
 
-  // Public - beneficiary getter
-  pub fn get_beneficiary(&self) -> AccountId {
-    self.beneficiary.clone()
-  }
+    //getters and setters
 
-  // Public - but only callable by env::current_account_id(). Sets the beneficiary
-  #[private]
-  pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
-    self.beneficiary = beneficiary;
-  }
-}
+    pub fn get_beneficiary(&self) -> AccountId {
+        self.beneficiary.clone()
+    }
 
+    //#[privileged_func_call]
+    pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
+        let caller = env::predecessor_account_id();
+        log!(
+        "called chang_beneficiary (privileged function), calling_account is {}, and privileged_account is currently {:?}",
+        &caller,
+        &self.privileged_account
+    );
+        if self.privileged_account.clone() != caller {
+            panic!(
+                "method access denied - no NFT in account - trying running privilege check first"
+            );
+        }
+        log!(
+            "from change_ben, at the beginngin self.ben is {} and ben is {}",
+            self.beneficiary,
+            beneficiary
+        );
+        self.beneficiary = beneficiary;
+    }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-  use near_sdk::testing_env;
-  use near_sdk::test_utils::VMContextBuilder;
-  use near_sdk::Balance;
+    pub fn get_nft_deployed_account(&self) -> AccountId {
+        self.nft_deployed_account.clone()
+    }
 
-  const BENEFICIARY: &str = "beneficiary";
-  const NEAR: u128 = 1000000000000000000000000;
-
-  #[test]
-  fn initializes() {
-      let contract = Contract::init(BENEFICIARY.parse().unwrap());
-      assert_eq!(contract.beneficiary, BENEFICIARY.parse().unwrap())
-  }
-
-  #[test]
-  fn donate() {
-      let mut contract = Contract::init(BENEFICIARY.parse().unwrap());
-
-      // Make a donation
-      set_context("donor_a", 1*NEAR);
-      contract.donate();
-      let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
-
-      // Check the donation was recorded correctly
-      assert_eq!(first_donation.total_amount.0, 1*NEAR);
-
-      // Make another donation
-      set_context("donor_b", 2*NEAR);
-      contract.donate();
-      let second_donation = contract.get_donation_for_account("donor_b".parse().unwrap());
-
-      // Check the donation was recorded correctly
-      assert_eq!(second_donation.total_amount.0, 2*NEAR);
-
-      // User A makes another donation on top of their original
-      set_context("donor_a", 1*NEAR);
-      contract.donate();
-      let first_donation = contract.get_donation_for_account("donor_a".parse().unwrap());
-
-      // Check the donation was recorded correctly
-      assert_eq!(first_donation.total_amount.0, 1*NEAR * 2);
-
-      assert_eq!(contract.number_of_donors(), 2);
-  }
-
-  // Auxiliar fn: create a mock context
-  fn set_context(predecessor: &str, amount: Balance) {
-    let mut builder = VMContextBuilder::new();
-    builder.predecessor_account_id(predecessor.parse().unwrap());
-    builder.attached_deposit(amount);
-
-    testing_env!(builder.build());
-  }
+    pub fn get_privileged_account(&self) -> AccountId {
+        self.privileged_account.clone()
+    }
 }
